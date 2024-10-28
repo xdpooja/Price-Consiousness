02838c37558586ae94a3b74c49128c19b4d4cbd1
Pooja <101863282+xdpooja@users.noreply.github.com>
2024-10-28 19:01:26 +0530

    Update Price Consiousness.py

--- pages/Price Consiousness.py
+++ pages/Price Consiousness.py
@@ -10,24 +10,40 @@ toggle = st.toggle('Slider options On/Off', value=True)
 if toggle:
     col11, col22 = st.columns(2)
     with col11:
-        slider_preference = st.selectbox('Select slider filter', options=['payment preference', 'shopping preference', 'planned or preplanned'])
+        slider_preference = st.selectbox('Select slider filter', options=['Payment Preference', 'Shopping Preference', 'Spontaneous or Preplanned', 'Transaction Frequency'])
     with col22:
+        slider_value = [1,2,3,4,5]
         x = []
 
-        if slider_preference == 'payment preference':
+        if slider_preference == 'Payment Preference':
             x.append('Cash')
+            x.append('Moderately Indifferent')
             x.append('Indifferent')
+            x.append('Moderately Digital')
             x.append('Digital')
-        if slider_preference == 'shopping preference':
+        if slider_preference == 'Shopping Preference':
             x.append('Offline')
+            x.append('Moderately Indifferent')
             x.append('Indifferent')
+            x.append('Moderately Online')
             x.append('Online')
-        if slider_preference == 'planned or preplanned':
+        if slider_preference == 'Spontaneous or Preplanned':
             x.append('Spontaneous')
+            x.append('Moderately Indifferent')
             x.append('Indifferent')
+            x.append('Moderately Pre Planned')
             x.append('Pre Planned')
+        if slider_preference == 'Transaction Frequency':
+            x.append('Less frequent with multiple products in one high value transaction')
+            x.append('Moderately Indifferent')
+            x.append('Indifferent')
+            x.append('Moderately Frequent')
+            x.append('Frequent with lesser products in multiple low value transactions')
 
-        slider = st.select_slider(f'Scale for {slider_preference}', options=x)
+        def stringify(i:int = 0) -> str:
+            return x[i-1]
+
+        slider = st.select_slider(f'Scale for {slider_preference}', options=slider_value, format_func=stringify)
 
     data = data_[data_[slider_preference]==slider]
 if not toggle:
